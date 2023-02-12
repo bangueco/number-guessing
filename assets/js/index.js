59ce81d6d76c1056763bea33bf8f08aa4336c3d5
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-02-12 10:41:24 +0800

    Add player turns

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
 // Queries
+let playerTurns = 10;
+let turns = document.querySelector("#turns");
 let guess_me;
 let guess_input = document.querySelector("#guessInput");
 let result = document.querySelector("#result");
@@ -13,12 +15,21 @@ function guessGameStart() {
   if (guess_input.valueAsNumber < guess_me) {
     result.style.color = "red";
     result.textContent = "Too low!";
+    playerTurns--;
+    turns.textContent = `Turns: ${playerTurns}`;
   } else if (guess_input.valueAsNumber > guess_me) {
     result.textContent = "Too high!";
     result.style.color = "green";
+    playerTurns--;
+    turns.textContent = `Turns: ${playerTurns}`;
   } else if (guess_input.valueAsNumber === guess_me) {
     result.style.color = "rgb(5, 201, 152)";
     result.textContent = "You guessed the correct number!";
+    turns.textContent = `Turns: ${playerTurns}`;
+  }
+
+  if (playerTurns <= 0) {
+    result.textContent = "You ran out of turns! Game Over!";
   }
 }
 
