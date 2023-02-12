9387a362add024cb9401b3e0089d330a158b4d06
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-02-12 11:12:30 +0800

    Add function to add low and high guessed numbers below

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,8 @@ let guess_me;
 let guess_input = document.querySelector("#guessInput");
 let result = document.querySelector("#result");
 let submit = document.querySelector("#submit");
+let lowNum = document.querySelector("#low_num");
+let highNum = document.querySelector("#high_num");
 let reset = document.querySelector("#reset");
 
 function randomizer() {
@@ -17,19 +19,31 @@ function guessGameStart() {
     result.textContent = "Too low!";
     playerTurns--;
     turns.textContent = `Turns: ${playerTurns}`;
+    let p = document.createElement("p");
+    p.style.color = "red";
+    lowNum.appendChild(p);
+    p.textContent = `${guess_input.valueAsNumber}`;
   } else if (guess_input.valueAsNumber > guess_me) {
     result.textContent = "Too high!";
     result.style.color = "green";
     playerTurns--;
     turns.textContent = `Turns: ${playerTurns}`;
-  } else if (guess_input.valueAsNumber === guess_me) {
-    result.style.color = "rgb(5, 201, 152)";
-    result.textContent = "You guessed the correct number!";
-    turns.textContent = `Turns: ${playerTurns}`;
+    let p = document.createElement("p");
+    p.style.color = "green";
+    highNum.appendChild(p);
+    p.textContent = `${guess_input.valueAsNumber}`;
   }
 
   if (playerTurns <= 0) {
     result.textContent = "You ran out of turns! Game Over!";
+    submit.disabled = true;
+  }
+
+  if (guess_input.valueAsNumber === guess_me) {
+    result.style.color = "rgb(5, 201, 152)";
+    result.textContent = "You guessed the correct number!";
+    turns.textContent = `Turns: ${playerTurns}`;
+    submit.disabled = true;
   }
 }
 
